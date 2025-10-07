ba2b0cdeac4a194279827b2695880738503a8bdc
Noelia-Rivera <noelia.rivera@upeu.edu.pe>
2025-10-07 01:46:57 -0500

    tablas implementadas

---
+++ src/main/java/com/mayadent/MAYADENTBD/entity/MetodoPago.java
@@ -0,0 +1,31 @@
+package com.mayadent.MAYADENTBD.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "metodo_pagos")
+@Builder
+public class MetodoPago {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "estado")
+    private char estado;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "metodoPago")
+    @JsonIgnore
+    private Set<Factura> factura;
+}
